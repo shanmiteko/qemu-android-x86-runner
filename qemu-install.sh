bdae0a424b0d97560358276b7d0dabe0c0a1cf43
shanmite <shanmite@hotmail.com>
2023-07-13 15:28:23 +0800

    fix

--- qemu-install.sh
+++ qemu-install.sh
@@ -6,8 +6,8 @@ qemu-img create -f qcow2 "$2" 4G
 
 qemu-system-x86_64 -enable-kvm -vga std \
                    -m 2048 -smp 2 -cpu host \
-                   -soundhw ac97 \
-                   -net nic,model=e1000 -net user \
+                   -net nic \
+		   -net user,hostfwd=tcp::4444-:5555 \
                    -cdrom "$1" \
                    -hda "$2" \
                    -boot d \
--- qemu-run.sh
+++ qemu-run.sh
@@ -8,9 +8,8 @@ shift
 
 qemu-system-x86_64 -enable-kvm -vga std \
                    -m 2048 -smp 2 -cpu host \
-                   -soundhw ac97 \
-                   -device e1000,netdev=net0 \
-                   -netdev user,id=net0 \
+		   -net nic \
+		   -net user,hostfwd=tcp::4444-:5555 \
                    -hda "$HDA_IMAGE" \
                    -monitor stdio \
                    "$@"
